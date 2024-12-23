d14fb7a272192bccb59145e9afacf60589e49f92
Reto Gerber <reto.gerber@uzh.ch>
2024-12-23 13:09:52 +0100

    fix TMA_location usage determination

--- workflow/scripts/Microscopy/create_postIMSpreIMSmask_geojson_presence.sh
+++ workflow/scripts/Microscopy/create_postIMSpreIMSmask_geojson_presence.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-while getopts a:b:o: flag
+while getopts a:b:g:o: flag
 do
     case "${flag}" in
-        # i) input_dir=${OPTARG};;
         a) postIMS_location=${OPTARG};;
         b) preIMS_location=${OPTARG};;
+        g) generic_file=${OPTARG};;
         o) output_file=${OPTARG};;
     esac
 done
@@ -21,18 +21,25 @@ if [ -z "$preIMS_location" ]; then
     exit 1
 fi
 
+if [ -z "$generic_file" ]; then
+    echo "Variable generic_file (flag -g) is empty!" >&2
+    exit 1
+fi
+
 if [ -z "$output_file" ]; then
     echo "Variable output_file (flag -o) is empty!" >&2
     exit 1
 fi
-echo $output_file
-echo $input_dir
+echo "postIMS_location: ${postIMS_location}"
+echo "preIMS_location: ${preIMS_location}"
+echo "generic_file: ${generic_file}"
+echo "output_file: ${output_file}"
 
 echo "type,exists" > $output_file
 
 # check if file exists
 #postIMS_location=$( find $input_dir -type f -name *TMA_mask_on_postIMS.geojson )
-if [ -f $postIMS_location ]; then
+if [ -f $postIMS_location ] && [ $postIMS_location != $generic_file ]; then
     echo "postIMS,1" >> $output_file
 else
     echo "postIMS,0" >> $output_file
@@ -40,7 +47,7 @@ fi
 
 # check if file exists
 #preIMS_location=$( find $input_dir -type f -name *TMA_mask_on_preIMS.geojson )
-if [ -f $preIMS_location ]; then
+if [ -f $preIMS_location ] && [ $preIMS_location != $generic_file ]; then
     echo "preIMS,1" >> $output_file
 else
     echo "preIMS,0" >> $output_file
